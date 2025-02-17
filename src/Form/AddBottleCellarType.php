7bce1daa451e21ff302eb72848dc7a13a684641e
Gipsy Rouaret <gipsy.rouaret@orange.fr>
2025-02-17 15:22:17 +0100

    catalogue + choix cellar operationnels

--- src/Form/AddBottleCellarType.php
+++ src/Form/AddBottleCellarType.php
@@ -2,33 +2,42 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Bottle;
 use App\Entity\Cellar;
-use App\Entity\User;
 use App\Repository\CellarRepository;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class AddBottleCellarType extends AbstractType
 {
+	private TokenStorageInterface  $tokenStorage;
+
+	public function __construct(TokenStorageInterface  $tokenStorage)
+	{
+	    $this->tokenStorage  = $tokenStorage;
+	}
+	
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
 	   ->add('cellar', EntityType::class, [
 		'class' => Cellar::class,
-		// 'choices' => $options['cellar'], // Liste des caves de l'utilisateur
 		'choice_label' => 'name', // Affiche le nom de la cave
 		'label' => 'Choisissez votre cave',
 		"multiple"=>false,
 		"expanded"=>false,
-		'query_builder' => function (CellarRepository $cellarRepository) {
+		'query_builder' => function (CellarRepository $cellarRepository){
+			$user = $this->tokenStorage->getToken()?->getUser();
 			return $cellarRepository->createQueryBuilder('c')
 			// récupère les caves de l'utilisateur
 			->join("c.user",'u')
 			->andwhere('u.id = :id') 
-			->setParameter('id', ["user"]);
+			->setParameter('id', $user->getId());
 		}
 	]);
 	// dd($options["user"]);
